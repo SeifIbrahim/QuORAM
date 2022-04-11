3582c0878b49a47a73355cca7f0696c42aa88ba5
Seif Ibrahim <seifibrahim10@gmail.com>
2022-04-11 11:30:49 -0700

    update guava version for lynch scripts

--- scripts/run-insecure-client.sh
+++ scripts/run-insecure-client.sh
@@ -1,1 +1,1 @@
-(cd target && java -Xmx30G -cp ../lib/commons-math3-3.6.1.jar:../lib/guava-19.0.jar:TaoServer-1.0-SNAPSHOT.jar TaoClient.InsecureTaoClient --runType load_test --clients $1 --load_test_length $2 --rwRatio $3 --zipfExp $4 --warmup_operations $5)
+(cd target && java -Xmx30G -cp ../lib/commons-math3-3.6.1.jar:../lib/guava-31.1-jre.jar:TaoServer-1.0-SNAPSHOT.jar TaoClient.InsecureTaoClient --runType load_test --clients $1 --load_test_length $2 --rwRatio $3 --zipfExp $4 --warmup_operations $5)
